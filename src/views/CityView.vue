63a63b5efaeb907dcdf686de5ff7d22db0a98797
Huy Vo <hvo@gsd-software.com>
2024-03-16 16:15:38 +0100

    upload code

---
+++ src/views/CityView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import AsyncCityView from "../components/AsyncCityView.vue";
+import CityViewSkeleton from "../components/CityViewSkeleton.vue";
+</script>
+
+<template>
+    <div>
+        <Suspense>
+            <template #default>
+                <AsyncCityView />
+            </template>
+            <template #fallback>
+                <CityViewSkeleton />
+            </template>
+        </Suspense>
+    </div>
+</template>
+
